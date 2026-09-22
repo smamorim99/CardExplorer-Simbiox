479b9ab783ece043021d037878aed8759b3b422b
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2026-09-22 16:42:57 -0300

    feat: remove test text from App component and clean up AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div>
       <AppRoutes />
-
-      test do app
     </div>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,12 @@ import { Route, Routes } from "react-router";
 import { CardSearch } from "../pages/CardSearch.tsx";
 import { CardDetails } from "../pages/CardDetails/index.tsx";
 
-
 export function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<CardSearch />} />
             <Route path="/search" element={<CardSearch />} />
             <Route path="/detail/:name" element={<CardDetails />} />
-
         </Routes>
     );
 }
